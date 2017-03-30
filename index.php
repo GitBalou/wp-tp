39245ffe496e6e5d16602280ed73aa523f7b75f0
Typhanie Bretéché <b.typh@hotmail.fr>
2017-03-30 13:29:37 +0200

    modif index.php

--- index.php
+++ index.php
@@ -1,20 +1,32 @@
  
 <?php get_header(); ?>
 
+<div id="content">
 <!--vérifier s’il y a des articles à afficher: « if have posts ». S’il n’y en a pas, il n’affiche rien. -->
 <?php if(have_posts()) : ?>
 
 <!--va aller chercher PARMI les articles (commande « while ») celui qu’il faut (« the post »)-->
 <?php while(have_posts()) : the_post(); ?> 
 
+<!--aller chercher l’ID de l’article dans la base-->
+<div class="post" id="post-<?php the_ID(); ?>">
+
 <!--appeler le titre de l’article ou des articles // avec commande qui va permettre au titre de s’afficher quand vous survolez son lien-->
 <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 
+<p class="postmetadata">
+<?php the_time('j F Y') . ' par ' . the_author() . comments_popup_link('Pas de commentaires', '1 Commentaire', '% Commentaires') . edit_post_link('Editer', ' &#124; ', ''); ?>   
+</p>
+
+<div class="post_content">
+<?php the_content(); ?>
+</div> 
+</div>
+
 <!--fermer cette requête-->
 <?php endwhile; ?> 
-<?php the_content(); ?>
 <?php endif; ?>
-
+</div>
 	</body>
 
 </html>
